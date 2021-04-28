728b969744993514da86aca1497681051223bcc7
Lukas <lukas.hengst@ta-systeme.com>
2021-04-28 13:52:41 +0200

    jetzt erst gelesen, dass nur die dht22-Datei ausgelesen werden soll und dementsprechend korrigiert. Upsi.

--- programm.py
+++ programm.py
@@ -1,12 +1,6 @@
 import csv
 
-reader1 = csv.reader(open("2021-04-26_dht22_sensor_3660.csv"), delimiter=";")
-reader2 = csv.reader(open("2021-04-26_sds011_sensor_3659.csv"), delimiter=";")
+reader = csv.reader(open("2021-04-26_dht22_sensor_3660.csv"), delimiter=";")
 
-print("DHT22:")
-for row in reader1:
-	print(row)
-
-print("SDS011:")
-for row in reader2:
+for row in reader:
 	print(row)
